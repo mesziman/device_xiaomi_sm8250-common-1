fd235c905297f99959522612358d2ae4905f0f00
ejbtrd <ejbtrd@gmail.com>
2021-06-06 21:09:53 +0200

    [TMP] sm8250-common: Switch to QTI BT stack
    Signed-off-by: ejbtrd <ejbtrd@gmail.com>

--- kona.mk
+++ kona.mk
@@ -127,6 +127,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    BluetoothQti \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
