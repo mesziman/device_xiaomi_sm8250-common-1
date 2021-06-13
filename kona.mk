123f3c7756e225dc3e9e9191ef9d6129906a32c6
ejbtrd <ejbtrd@gmail.com>
2021-06-13 09:09:46 +0200

    [TMP] sm8250-common: Build vendor.qti.hardware.btconfigstore@2.0
    Signed-off-by: ejbtrd <ejbtrd@gmail.com>

--- kona.mk
+++ kona.mk
@@ -129,7 +129,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     BluetoothQti \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
